2f00644565e2758e2d2f9d09d72e6ff2793f3ee1
djunh1 <djunh1@gmail.com>
2025-09-25 16:12:57 -0400

    Add various user actions

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from .custom_jwt_claims import CustomTokenObtainPairView
 
-from users.views import UserView, NewUserView
+from users.views import ResetLoginAttemptsView, UpdateUserStatusView, DeleteUserView, UpdateUserView, UserView, NewUserView, AdminResetUserPasswordView, ResetUserPasswordView
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -11,7 +11,11 @@ router.register(r'users', UserView)
 urlpatterns = [
     path('', include(router.urls)),
     path('users/new', NewUserView.as_view(), name='new_user'),
+    path('users/delete', DeleteUserView.as_view(), name='delete_user'),
+    path('users/update-status', UpdateUserStatusView.as_view(), name='update_user_status'),
+    path('users/update', UpdateUserView.as_view(), name='update_user'),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-
-
+    path('users/admin-reset-login-attempts', ResetLoginAttemptsView.as_view(), name="unblock"),
+    path('users/admin-reset-password', AdminResetUserPasswordView.as_view(), name="admin_reset_password"),
+    path('users/user-reset-password',ResetUserPasswordView.as_view(), name='reset_password'),
 ]
--- users/views.py
+++ users/views.py
@@ -6,8 +6,14 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
+from django.core.exceptions import ValidationError
+
+from axes.utils import reset
+
 from users.models import User
 from users.serializer import UserSerializer
+
+from django.contrib.auth.password_validation import validate_password
 
 logger = logging.getLogger(__name__)
 
@@ -19,8 +25,9 @@ class UserView(ModelViewSet):
 
 class NewUserView(APIView):
     permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
-
+    # todo email as login?
     def post(self, request, format=None):
+
         data = request.data
         user = User(username=data['username'], 
                     first_name=data['firstName'], 
@@ -33,9 +40,126 @@ class NewUserView(APIView):
         user.set_password(data['passwd'])
 
         try:
+            validate_password(data['passwd'], user)
             user.save()
+        except ValidationError as e:
+            return Response(e, status=status.HTTP_400_BAD_REQUEST)
         except Exception:
             error_message = User._meta.get_field('username').error_messages['unique']
             return Response({'error': error_message}, status=status.HTTP_406_NOT_ACCEPTABLE)
         
-        return Response({}, status=status.HTTP_200_OK)
+        return Response({"good"}, status=status.HTTP_200_OK)
+
+class DeleteUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
+    def delete(self, request, format=None):
+        data = request.data
+        user = User.objects.get(id=data['user_id'])
+        user.delete()
+
+        return Response({"good"}, status=status.HTTP_200_OK)
+    
+class UpdateUserStatusView(APIView):
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
+    def post(self, request, format=None):
+        data = request.data
+        user = User.objects.get(id=data['user_id'])
+
+        user.is_active = data['active_status']
+        user.save()
+
+        response_string = f"User Status Enabled = {data['active_status']}"
+
+        return Response({ response_string }, status=status.HTTP_200_OK)
+    
+
+class UpdateUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, request):
+        data = request.data
+        if self.checkif_request_field_valid(request, "username"):
+            request.user.username = data["username"]
+        if self.checkif_request_field_valid(request, "first_name"):
+            request.user.first_name = data["first_name"]
+        if self.checkif_request_field_valid(request, "last_name"):
+            request.user.last_name = data["last_name"]
+
+        try:
+            request.user.save()
+
+        except Exception:
+            error_message =  User._meta.get_field('username').error_messages['unique']
+            return Response({"error": error_message}, status=status.HTTP_406_NOT_ACCEPTABLE)
+       
+        return Response({"good"}, status=status.HTTP_200_OK)
+    
+    def checkif_request_field_valid(self, request, field_name):
+        if field_name in request.data and request.data[field_name] and request.data[field_name] != "":
+            return True
+        return False
+
+
+class ResetLoginAttemptsView(APIView):
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
+    def post(self, request):
+        data = request.data
+
+        if 'blocked_user' not in data.keys():
+            raise ValidationError(detail={'blocked_user': 'Field is required'})
+        
+        if not User.objects.filter(username=data['blocked_user']).exists():
+            raise ValidationError(('Username does not exist.'), code='Username does not exist.')
+        
+        reset(username=data['blocked_user'])
+
+        return Response({"User is unblocked successfully"}, status=status.HTTP_200_OK)
+
+class AdminResetUserPasswordView(APIView):
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+
+    def post(self, request):
+        if not request.user.is_staff:
+            return Response({'response': "User is not an admin."}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        data = request.data
+        new_password = data['new_passwd']
+
+        target_user = data['target_user']
+
+        validate_password(new_password, target_user)
+
+        user = User.objects.get(username=target_user)
+        user.required_password_change = False
+        user.password_change_date = timezone.now()
+        user.set_password(new_password)
+        user.save()
+
+        return Response({'response' :'good'}, status=status.HTTP_200_OK)
+
+class ResetUserPasswordView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request): #, format=None
+        try:
+            data = request.data
+            new_passwd = data["new_passwd"]
+            confirm_passwd = data["confirm_passwd"]
+
+            if (new_passwd != confirm_passwd):
+                raise ValidationError(("Password should match"),code='Password should match')
+
+            validate_password(new_passwd, request.user)
+            
+        except ValidationError as e:
+            return Response({"errors": e.error_list}, status=status.HTTP_403_FORBIDDEN)
+
+        request.user.required_password_change = False
+        request.user.password_change_date = timezone.now()
+        request.user.set_password(new_passwd)
+        request.user.save()
+
+        return Response({"response": "Successfully updated password"}, status=status.HTTP_200_OK)
